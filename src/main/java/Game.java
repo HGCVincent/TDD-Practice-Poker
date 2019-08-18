a92a589459bcc69fd3fc90a23b5d8a4fb96c1982
Vincent <1061271330@qq.com>
2019-08-18 14:28:35 +0800

    [HGC]Case 3
    should_return_player1_win_when_player1_vs_player2_given_2H_3D_8S_9C_KD_vs_2H_3D_5S_9C_KD

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,10 +1,18 @@
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
 public class Game {
     public String play(Player player1, Player player2) {
-        if (player1.getPoker().getNumber() > player2.getPoker().getNumber()){
-            return player1.getName() + " win";
+        player1.setPokerDtos(player1.getPokerDtos().stream().sorted(Comparator.comparing(PokerDto::getNumber).reversed()).collect(Collectors.toList()));
+        player2.setPokerDtos(player2.getPokerDtos().stream().sorted(Comparator.comparing(PokerDto::getNumber).reversed()).collect(Collectors.toList()));
+        for (int i = 0; i < 5; i++){
+            if (player1.getPokerDtos().get(i).getNumber() > player2.getPokerDtos().get(i).getNumber()){
+                return player1.getName() + " win";
+            }
+            else if (player1.getPokerDtos().get(i).getNumber() < player2.getPokerDtos().get(i).getNumber()){
+                return player2.getName() + " win";
+            }
         }
-        else {
-            return player2.getName() + " win";
-        }
+        return "Equalize";
     }
 }
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,17 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Player {
     String name;
-    PokerDto poker;
+    List<PokerDto> pokerDtos = new ArrayList<>();
 
-    public Player(String name, Poker poker) {
+    public Player(String name, Poker... pokers) {
         this.name = name;
-        this.poker =new PokerDto(poker);
+        List<Poker> pokerList = Arrays.asList(pokers);
+        for (Poker poker : pokers) {
+            pokerDtos.add(new PokerDto(poker));
+        }
     }
 
     public String getName() {
         return name;
     }
 
-    public PokerDto getPoker() {
-        return poker;
+    public List<PokerDto> getPokerDtos() {
+        return pokerDtos;
+    }
+
+    public void setPokerDtos(List<PokerDto> pokerDtos) {
+        this.pokerDtos = pokerDtos;
     }
 }
